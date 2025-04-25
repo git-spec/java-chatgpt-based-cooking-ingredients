d7c97c5b91bc46b73eb19404282258839348e9c9
git-spec <inkyfischer@gmail.com>
2025-04-26 00:01:24 +0200

    feat: Create categorizeIngredient in IngredientController.

--- src/main/java/com/example/chatgptbasedcookingingredients/IngredientController.java
+++ src/main/java/com/example/chatgptbasedcookingingredients/IngredientController.java
@@ -1,5 +1,6 @@
 package com.example.chatgptbasedcookingingredients;
 
+import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,17 +8,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.chatgptbasedcookingingredients.model.OpenAIMessage;
+import com.example.chatgptbasedcookingingredients.model.OpenAIRequest;
+import com.example.chatgptbasedcookingingredients.model.OpenAIResponse;
+
 @RestController
 @RequestMapping("/ingredients")
 @RequiredArgsConstructor
 public class IngredientController {
+    private final IngredientService service;
 
     @PostMapping
-    String categorizeIngredient(@RequestBody String ingredient) {
-
-        // TODO: This should return "vegan", "vegetarian" or "regular" depending on the ingredient.
-
-        return "vegan";
+    public String categorizeIngredient(@RequestBody String ingredient) {
+        OpenAIMessage message = new OpenAIMessage(
+            "user", 
+            ingredient + ": Is it vegan, vegeterian or normal? Please select only one of this words."
+        );
+        List<OpenAIMessage> messages = List.of(message);
+        OpenAIRequest request = new OpenAIRequest("gpt-4.1", messages);
+        OpenAIResponse response = service.getFoodType(request);
+        return response.choices().getFirst().message().content();
     }
 
 }
