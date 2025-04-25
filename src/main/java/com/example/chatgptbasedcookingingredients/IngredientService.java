65360c2e98512843dcc4241d4bb023f2b04d908f
git-spec <inkyfischer@gmail.com>
2025-04-26 00:00:29 +0200

    feat: Create getFoodType in IngredientService.

--- src/main/java/com/example/chatgptbasedcookingingredients/IngredientService.java
+++ src/main/java/com/example/chatgptbasedcookingingredients/IngredientService.java
@@ -1,18 +1,31 @@
 package com.example.chatgptbasedcookingingredients;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import com.example.chatgptbasedcookingingredients.model.OpenAIRequest;
+import com.example.chatgptbasedcookingingredients.model.OpenAIResponse;
 
 
 @Service
 public class IngredientService {
     RestClient restClient;
 
-    public IngredientService(RestClient.Builder clientBuilder, @Value("API_KEY") String apiKey) {
+    public IngredientService(RestClient.Builder clientBuilder, @Value("${API_KEY}") String apiKey) {
         this.restClient = clientBuilder
             .baseUrl("https://api.openai.com/v1/chat/completions")
-            .defaultHeader("Authorisation", "Bearer ", apiKey)
+            .defaultHeader("Authorization", "Bearer " + apiKey)
             .build();
+    }
+
+    public OpenAIResponse getFoodType(OpenAIRequest request) {
+        return restClient
+            .post()
+            .accept(MediaType.APPLICATION_JSON)
+            .body(request)
+            .retrieve()
+            .body(OpenAIResponse.class);
     }
 }
